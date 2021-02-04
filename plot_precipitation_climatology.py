9fe5d635b47419fc51f97f338c0d2689aaac2a22
Zak Baillie <zak.baillie@anu.edu.au>
2021-02-04 13:39:51 +1100

    Initial commit of precip script

---
+++ plot_precipitation_climatology.py
@@ -0,0 +1,86 @@
+import argparse
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import numpy as np
+import cmocean
+
+
+def convert_pr_units(darray):
+    """Convert kg m-2 s-1 to mm day-1.
+    
+    Args:
+      darray (xarray.DataArray): Precipitation data
+    
+    """
+    
+    darray.data = darray.data * 86400
+    darray.attrs['units'] = 'mm/day'
+    
+    return darray
+
+
+def create_plot(clim, model, season, gridlines=False, levels=None):
+    """Plot the precipitation climatology.
+    
+    Args:
+      clim (xarray.DataArray): Precipitation climatology data
+      model (str): Name of the climate model
+      season (str): Season
+      
+    Kwargs:
+      gridlines (bool): Select whether to plot gridlines    
+    
+    """
+    
+    if not levels:
+        levels = np.arange(0,13.5,1.5)
+        
+    fig = plt.figure(figsize=[12,5])
+    ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
+    clim.sel(season=season).plot.contourf(ax=ax,
+                                          levels=levels,
+                                          extend='max',
+                                          transform=ccrs.PlateCarree(),
+                                          cbar_kwargs={'label': clim.units},
+                                          cmap=cmocean.cm.haline_r)
+    ax.coastlines()
+    if gridlines:
+        plt.gca().gridlines()
+    
+    title = f'{model} precipitation climatology ({season})'
+    plt.title(title)
+
+
+def main(inargs):
+    """Run the program."""
+
+    dset = xr.open_dataset(inargs.pr_file)
+    
+    clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
+    clim = convert_pr_units(clim)
+
+    create_plot(clim, dset.attrs['source_id'], inargs.season, 
+                gridlines=inargs.gridlines, levels=inargs.cbar_levels)
+    plt.savefig(inargs.output_file, dpi=200)
+
+
+if __name__ == '__main__':
+    description='Plot the precipitation climatology.'
+    parser = argparse.ArgumentParser(description=description)
+    
+    parser.add_argument("pr_file", type=str, help="Precipitation data file")
+    parser.add_argument("season", type=str, 
+                        choices=['DJF','MAM','JJA','SON'], 
+                        help="Season to plot")
+    parser.add_argument("output_file", type=str, help="Output file name")
+    parser.add_argument("--gridlines", default=False, 
+                        help="Optional gridlines",
+                        action="store_true")
+    parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
+                        help='list of levels / ticks marks to appear on the colourbar')
+
+    args = parser.parse_args()
+    
+    main(args)
+    
